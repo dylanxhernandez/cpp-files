602b815a3f2556bc49588c4edea6014e5c58f639
dylanxhernandez <dylan.x.h.developer@gmail.com>
2023-09-07 00:49:12 -0400

    feat: add basic script to pull memory and disk info

---
+++ memory_check/memtest.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <sys/statvfs.h>
+
+double getMemoryUsage() {
+	struct sysinfo memInfo;
+	sysinfo(&memInfo);
+	return (double)(memInfo.totalram - memInfo.freeram) * memInfo.mem_unit / (1024 * 1024); // in MB
+}
+
+double getDiskUsage(const std::string &path) {
+	struct statvfs diskInfo;
+	statvfs(path.c_str(), &diskInfo);
+	return (double)(diskInfo.f_blocks - diskInfo.f_bfree) * diskInfo.f_frsize / (1024 * 1024); // in MB
+}
+
+int main() {
+	// Get memory and disk usage and assign to variables
+	double memoryUsed = getMemoryUsage();
+	double diskUsed = getDiskUsage("/");
+
+	// Display the information in a table
+	std::cout << "--------------------------------------" << std::endl;
+	std::cout << "| Memory Used | Disk Used |" << std::endl;
+	std::cout << "--------------------------------------" << std::endl;
+	std::cout << "| " << std::fixed << std::setprecision(2) << memoryUsed << " MB   | " << diskUsed << " MB  |" << std::endl;
+	std::cout << "--------------------------------------" << std::endl;
+
+	return 0;
+}
+
